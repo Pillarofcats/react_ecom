8d528e5f7ff939f7024b4e0dffe99e5b2c6b9532
Pillarofcats <jacob.white.07@gmail.com>
2023-11-08 02:37:14 -0600

    Update for production.

--- frontend/hooks/useGetUserData.ts
+++ frontend/hooks/useGetUserData.ts
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from "../redux/hooks/default"
 import { getUserPurchases, getUserReviews } from "../redux/slices/userSlice"
 
 export default function useAuthUser() {
+  
   const dispatch = useAppDispatch()
   const user = useAppSelector((state) => state.user)
 
