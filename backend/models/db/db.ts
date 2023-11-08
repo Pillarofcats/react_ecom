b5a61aac520621ac25539be8830ffaf85520929d
Pillarofcats <jacob.white.07@gmail.com>
2023-11-08 03:06:17 -0600

    Fixed remote DB options.

--- backend/models/db/db.ts
+++ backend/models/db/db.ts
@@ -4,23 +4,23 @@ import { tParams } from "../../types/types.js"
 import pg from "pg"
 const { Pool } = pg
 
-const localDBOptions = {
-  user: process.env.PSQL_USER,
-  host: process.env.PSQL_HOST,
-  database: process.env.PSQL_DATABASE,
-  password: process.env.PSQL_PASSWORD,
-  port: Number(process.env.PSQL_PORT),
-}
-
-// const remoteDBOptions = {
-//   user: process.env.PGUSER,
-//   host: process.env.PGHOST,
-//   database: process.env.PGDATABASE,
-//   password: process.env.PGPASSWORD,
-//   port: Number(process.env.PGPORT),
+// const localDBOptions = {
+//   user: process.env.PSQL_USER,
+//   host: process.env.PSQL_HOST,
+//   database: process.env.PSQL_DATABASE,
+//   password: process.env.PSQL_PASSWORD,
+//   port: Number(process.env.PSQL_PORT),
 // }
 
-const pool = new Pool(localDBOptions)
+const remoteDBOptions = {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT),
+}
+
+const pool = new Pool(remoteDBOptions)
 
 const dbQuery = (text: string, params: tParams) => {
   return pool.query(text, params)
