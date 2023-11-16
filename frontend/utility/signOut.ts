2fb02d2aa0b5876fa3ee858c461aec3a356cf63a
Pillarofcats <jacob.white.07@gmail.com>
2023-11-15 19:54:44 -0600

    Fixed mobile sign-out.

--- frontend/utility/signOut.ts
+++ frontend/utility/signOut.ts
@@ -1,3 +1,3 @@
 export default function signOut() {
-  document.cookie = "3b_uid=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+  document.cookie = "3b_uid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; domain=.up.railway.app;"
 }
